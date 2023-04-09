846e761257f349890dd39af59943df171359905e
rakeem <rakeemshaikh4545@gmail.com>
2023-04-09 15:08:33 +0530

    adding the react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
-import Newz from './Newz'
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Newz from "./Newz";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Newz pageSize={6} country={'in'} category={'sports'}  />
+          <BrowserRouter>
+          <Navbar />
+            <Routes>
+              <Route exact path="/" element={<Newz pageSize={6} country={"in"} category={"general"} />} />
+              <Route exact path="/home" element={<Newz pageSize={6} country={"in"} category={"general"} />} />
+              <Route exact path="/general" element={<Newz pageSize={6} country={"in"} category={"general"} />} />
+              <Route exact path="/entertainment" element={<Newz pageSize={6} country={"in"} category={"entertainment"} />} />
+              <Route exact path="/business" element={<Newz pageSize={6} country={"in"} category={"business"} />} />
+              <Route exact path="/health" element={<Newz pageSize={6} country={"in"} category={"health"} />} />
+              <Route exact path="/science" element={<Newz pageSize={6} country={"in"} category={"science"} />} />
+              <Route exact path="/sports" element={<Newz pageSize={6} country={"in"} category={"sports"} />} />
+              <Route exact path="/technology" element={<Newz pageSize={6} country={"in"} category={"technology"} />} />
+            </Routes>
+          </BrowserRouter>
       </div>
-    )
+    );
   }
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
@@ -6,9 +7,9 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand text-primary" href="/">
+            <Link className="navbar-brand text-primary" to="/">
               Newz Monkey
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -26,49 +27,44 @@ export default class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="/">
+                  <Link className="nav-link" aria-current="page" to="/general">
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    About
-                  </a>
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    business
-                  </a>
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                {/* <li className="nav-item">
+                  <Link className="nav-link" to="/general">
+                    General
+                  </Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">
+                    Health
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    entertainment
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    general
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    health
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/science">
                     science
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    sports
-                  </a>
+                  <Link className="nav-link" to="/sports">
+                    Sports
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    technology
-                  </a>
+                  <Link className="nav-link" to="/technology">
+                    Technology
+                  </Link>
                 </li>
               </ul>
             </div>
